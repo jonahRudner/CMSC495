2a547da4c710ee70e8fdc2beaab97019c0967c78
Jonah <118635558+jonahRudner@users.noreply.github.com>
2024-09-08 10:18:49 -0400

    Update db.sql

--- App/db.sql
+++ App/db.sql
@@ -1,22 +1,25 @@
+--Create table that stores guest information
 CREATE TABLE Guests (
     GuestID INT PRIMARY KEY AUTO_INCREMENT,
     Name VARCHAR(255) NOT NULL,
     PhoneNumber VARCHAR(20),
     EmailAddress VARCHAR(255)
 );
-
+--Create table that stores room type information (ie large room, king suite, etc)
 CREATE TABLE RoomTypes (
     RoomTypeID INT PRIMARY KEY AUTO_INCREMENT,
     RoomTypeName VARCHAR(255) NOT NULL,
     Amenities TEXT
 );
 
+--Create table that stores physical rooms, and the id for which type they are
 CREATE TABLE Rooms (
     RoomID INT PRIMARY KEY AUTO_INCREMENT,
     RoomTypeID INT,
     FOREIGN KEY (RoomTypeID) REFERENCES RoomTypes(RoomTypeID)
 );
 
+--Create table that stores reservations, including the guest and which room
 CREATE TABLE Reservations (
     ReservationID INT PRIMARY KEY AUTO_INCREMENT,
     RoomID INT,
